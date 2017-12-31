d0087e51512c74ec74ea4ee924729aaf36c6a657
Killian Kemps <developer@killiankemps.fr>
2017-12-31 11:48:03 +0100

    Add signup on homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,23 @@
 class HomepageController < ApplicationController
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
+
   def index
     @users = User.all
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
