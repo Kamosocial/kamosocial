ea6c3e01a30f8db94cd451a90a4340aea54ba87f
Killian Kemps <developer@killiankemps.fr>
2017-12-31 14:28:13 +0100

    Make username case-insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :confirmable, :trackable, :validatable
 
-  validates :username, uniqueness: true
+  validates :username, uniqueness: { case_sensitive: false }
 
   has_many :interests
 end
