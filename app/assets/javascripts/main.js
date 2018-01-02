9bca4c1bb4725a88a700618849f883df7088673b
Killian Kemps <developer@killiankemps.fr>
2018-01-02 22:25:22 +0100

    WIP extract place from Leaflet and store it

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,27 @@
 console.log('Hey!');
+
+var addedPlaceEvent = document.createEvent('Event');
+addedPlaceEvent.initEvent('custom/addedPlace', true, true);
+
 const provider = new window.GeoSearch.OpenStreetMapProvider();
+
+function addPlaceButton() {
+  var event = new CustomEvent('custom/addedPlace');
+  window.dispatchEvent(event);
+}
+
+function popupCallback({query, result}) {
+  var event = new CustomEvent('custom/openedPopup', { detail: result });
+  window.dispatchEvent(event);
+  return `<div class="popup-label" data-place-label="${result}">${result.label}</div> <br /> <button id="add-place" onclick="${addPlaceButton} addPlaceButton();">Add</button>`
+}
 
 const searchControl = new window.GeoSearch.GeoSearchControl({
   provider: provider,
+  showPopup: true,
+  popupFormat: popupCallback,
   style: 'bar',
+  maxMarkers: 5,
   keepResult: true
 });
 
@@ -21,3 +39,15 @@ mymap.addControl(searchControl);
 mymap.on('geosearch/showlocation', function(event){
   console.log('Got an event: ', event);
 });
+
+var latestPlace = {};
+
+window.addEventListener('custom/openedPopup', function (e) {
+  console.log('Receivd opened popup event: ', e);
+  latestPlace = e.detail;
+}, false);
+
+window.addEventListener('custom/addedPlace', function (e) {
+  console.log('Received add place event, going to add ', latestPlace);
+  console.log(e);
+}, false);
