53585d6a9c26a1de4b293837f5111f915c61d743
Killian Kemps <developer@killiankemps.fr>
2018-01-04 22:33:52 +0100

    Manage values as is coming from OSM or directly from app

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,9 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :places
 
   def interests_list=value
+    if value.empty?
+      raise ArgumentError, 'Please add at least one interest'
+    end
     current_interest = value.split(',').collect{|interest| interest.strip.downcase}.uniq
     current_interest.each do |interest|
       self.interests << Interest.find_or_create_by(name: interest)
@@ -28,16 +31,23 @@ class User < ApplicationRecord
   end
 
   def places_list=value
+    if value.empty?
+      raise ArgumentError, 'Please add at least one place'
+    end
     current_place = JSON.parse(value)
     puts "currents place: #{current_place}"
     current_place.each do |place|
-      place['name'] = place['label'].split(',')[0]
+      place['name'] = place['label'].split(',')[0] unless place['name']
+      place['label'] = place['label'] unless place['address']
+      place['latitude'] = place['y'].to_f unless place['latitude']
+      place['longitude'] = place['x'].to_f unless place['longitude']
+      place['osm_id'] = place['raw']['osm_id'].to_i unless place['osm_id']
       puts "2.this place: #{place}"
       self.places << Place.create_with(name: place['name'],
                                         address: place['label'],
-                                        latitude: place['y'].to_f,
-                                        longitude: place['x'].to_f)
-        .find_or_create_by(osm_id: place['raw']['osm_id'].to_i)
+                                        latitude: place['latitude'],
+                                        longitude: place['longitude'])
+        .find_or_create_by(osm_id: place['osm_id'])
     end
   end
 
