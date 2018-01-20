06d63c847f0445505471c48128c0fa2d85d3724a
Killian Kemps <developer@killiankemps.fr>
2018-01-20 22:56:37 +0100

    Change email address for kamo.social

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "posteo.de",
+    address: "mail.gandi.net",
     port: 587,
     domain: ENV["EMAIL_DOMAIN"],
     authentication: "plain",
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "posteo.de",
+    address: "mail.gandi.net",
     port: 587,
     domain: ENV["EMAIL_DOMAIN"],
     authentication: "plain",
