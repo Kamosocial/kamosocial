82d0f4c181c262c9cc21a425b6ee830e235ca5ac
Killian Kemps <developer@killiankemps.fr>
2018-01-23 19:50:32 +0100

    Add locale translation base for welcome message

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Comminterest
     config.load_defaults 5.1
 
     config.i18n.available_locales = %w(en fr)
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
