3591034382664320fb86595c7a3f8dd8a99f9bef
Killian Kemps <developer@killiankemps.fr>
2018-01-27 11:57:43 +0100

    Add signup button on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     # devise_for :users
     devise_scope :user do
       get "/login" => "devise/sessions#new" # custom path to login/sign_in
-      get "/signup" => "devise/registrations#new" # custom path to sign_up/registration
+      get "/signup" => "devise/registrations#new", as: "signup" # custom path to sign_up/registration
     end
     devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'confirmation', unlock: 'unlock', registration: 'register', sign_up: 'sign_up' }
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
