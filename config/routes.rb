6ba9ddd75e476142b187295e0785cc5b1d745b94
Killian Kemps <developer@killiankemps.fr>
2018-01-28 12:55:51 +0100

    Redirect to login page after registration

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       get "/login" => "devise/sessions#new" # custom path to login/sign_in
       get "/signup" => "devise/registrations#new", as: "signup" # custom path to sign_up/registration
     end
-    devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'confirmation', unlock: 'unlock', registration: 'register', sign_up: 'sign_up' }
+    devise_for :users, controllers: { registrations: "registrations" }, path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'confirmation', unlock: 'unlock', registration: 'register', sign_up: 'sign_up' }
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     root 'homepage#index'
