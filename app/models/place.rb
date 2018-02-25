6511edc41cac1a5982a774ec093ab160506390db
Killian Kemps <developer@killiankemps.fr>
2018-02-25 21:58:07 +0100

    Add validation on places

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,5 @@ class Place < ApplicationRecord
   has_many :interests, through: :users
 
   validates :osm_id, uniqueness: { case_sensitive: false }
+  validates :osm_id, :name, :address, :latitude, :longitude, presence: true
 end
